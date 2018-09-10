85821bf5cb0880a8fa13aa7fed8df7f6827a4dab
Ythnam <phildesalondepce@hotmail.fr>
2018-09-10 19:15:37 +0200

    Adding 2 combobox and selecting Input and Output language

--- SpeechTranslator/ViewModel/MainViewModel.cs
+++ SpeechTranslator/ViewModel/MainViewModel.cs
@@ -5,6 +5,7 @@ using SpeechTranslator.Core.Model;
 using SpeechTranslator.Core.Strategy;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -16,8 +17,39 @@ namespace SpeechTranslator.ViewModel
     {
         #region Properties
         enum TranslationMode { ToText=1, ToSpeech=2 };
-        private  readonly StrategyContext Context;
-        private readonly List<LanguageModel> outputLanguages;
+        private readonly StrategyContext Context;
+        private readonly ObservableCollection<LanguageModel> Languages;
+
+        public ObservableCollection<LanguageModel> InputLanguages { get; }
+        public ObservableCollection<LanguageModel> OutputLanguages { get; }
+
+        private LanguageModel inputLanguge;
+        public LanguageModel InputLanguage
+        {
+            get { return this.inputLanguge; }
+            set
+            {
+                if (this.inputLanguge != value)
+                {
+                    this.inputLanguge = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private LanguageModel outputLanguge;
+        public LanguageModel OutputLanguage
+        {
+            get { return this.outputLanguge; }
+            set
+            {
+                if (this.outputLanguge != value)
+                {
+                    this.outputLanguge = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
 
         private string textFromSpeech;
         public string TextFromSpeech
@@ -51,13 +83,16 @@ namespace SpeechTranslator.ViewModel
         public MainViewModel()
         {
             this.Context = new StrategyContext();
-            this.outputLanguages = new List<LanguageModel>();
+            this.Languages = new ObservableCollection<LanguageModel>();
             var supportedLanguages = ConfigurationManager.GetSection(Configuration.SupportedLanguagesSection.SectionName) as Configuration.SupportedLanguagesSection;
             if (supportedLanguages != null)
             {
                 foreach (Configuration.LanguageElement langElement in supportedLanguages.Languages)
-                    this.outputLanguages.Add(new LanguageModel(langElement.Language, langElement.Code));
+                    this.Languages.Add(new LanguageModel(langElement.Language, langElement.Code));
             }
+
+            this.InputLanguages = new ObservableCollection<LanguageModel>(this.Languages);
+            this.OutputLanguages = new ObservableCollection<LanguageModel>(this.Languages);
         }
 
         private ICommand onStartEventCommand;
@@ -73,9 +108,11 @@ namespace SpeechTranslator.ViewModel
         {
             Context.Strategy = new SpeechStrategy(ConfigurationManager.AppSettings["KEY"], ConfigurationManager.AppSettings["AzureServer"]);
 
+            List<LanguageModel> outputLanguageSelected = new List<LanguageModel>();
+            outputLanguageSelected.Add(this.OutputLanguage);
             
 
-            List<LanguageModel> translations = await Context.TranslateToText(new LanguageModel("France", "fr-FR"), this.outputLanguages);
+            List<LanguageModel> translations = await Context.TranslateToText(this.InputLanguage, outputLanguageSelected);
 
             //List<LanguageModel> translations = await Context.TranslateToText("fr-FR", new List<string>() { "en-GB", "it-IT", "de-DE" });
 
